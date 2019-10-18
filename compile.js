8f62880b953b947f80c19a3feb758eeee17368d2
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-10-18 15:41:59 +0200

    compile the contract

---
+++ compile.js
@@ -0,0 +1,31 @@
+const solc = require('solc')
+const fs = require('fs')
+
+// const input = require('./contracts.json')
+
+
+const CONTRACT_NAME = 'Batcher.sol'
+const CONTRACT_FILE = `./contracts/${CONTRACT_NAME}`
+
+const content = fs.readFileSync(CONTRACT_FILE).toString()
+
+const input = {
+  language: 'Solidity',
+  sources: {
+    [CONTRACT_NAME]: {
+      content: content
+    }
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*']
+      }
+    }
+  }
+}
+console.log(input)
+const output = solc.compile(JSON.stringify(input))
+console.log(output)
+
+fs.writeFileSync(`./abis.json`, output)
